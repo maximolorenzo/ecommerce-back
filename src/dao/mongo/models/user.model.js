d782ad52a26cc8e2e5a9503e12f3645541304e88
maximolorenzoo <maximolorenzoo@gmail.com>
2023-06-15 00:44:22 -0300

    anda todo profe

--- src/dao/mongo/models/user.model.js
+++ src/dao/mongo/models/user.model.js
@@ -25,6 +25,10 @@ const userSchema = new mongoose.Schema({
     type: Date,
     default: Date.now(),
   },
+  cart: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "carts",
+  },
 });
 
 mongoose.set("strictQuery", false);
